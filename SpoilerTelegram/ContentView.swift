0a6a703c291547f286fde87bf72576b1aed4464c
githubuiux <uiuxista@gmail.com>
2023-06-25 20:45:17 +0300

    Initial Commit

---
+++ SpoilerTelegram/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpoilerTelegram
+//
+//  Created by Levent on 25.06.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpoilerTelegram/SpoilerTelegramApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpoilerTelegramApp.swift
+//  SpoilerTelegram
+//
+//  Created by Levent on 25.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct SpoilerTelegramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
